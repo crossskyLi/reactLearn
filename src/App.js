f4af4d16debca5483b5cf3edc1bc292e1c047bae
liguanliang <liguanliang@sensetime.com>
2020-04-09 17:07:38 +0800

    feat: add axios

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import List from './components/list'
 import Button from './components/button'
 import FuncCom from './components/funcCom'
 import RefDemo from './components/refDemo'
+import axios from 'axios'
 class App extends React.Component {
 
   funcMsg = "函数参数"
@@ -35,6 +36,43 @@ class App extends React.Component {
     const dataList = this.state.dataList.slice();
     dataList.push(!dataList[0] ? 1 : dataList[dataList.length - 1] + 1)
     this.setState({ dataList })
+
+    this.getData()
+  }
+
+  getData = () => {
+    const host = "http://127.0.0.1:3400"
+
+    const api = axios.create({
+      timeout: 60000,
+      baseURL: "//" + host,
+    });
+
+    const params = {
+      a: [
+        // 1,
+        // { dd: 11 },
+        // { a: "abckksdj大家看见", abc: [] },
+        [
+          { a: 1, b: { abccdd: 1 } },
+          ['abckk']
+        ]
+      ],
+      b: { c: 3 }
+      // b: {
+      //   c: 1,
+      //   aa: [
+      //     1,
+      //     3,
+      //     { a: 'abcd' }
+      //   ],
+      //   d: { a: 1 }
+      // }
+    }
+    //请求数据函数
+    api.get('/getUserList', { params }).then((res) => {
+      console.log(res)
+    })
   }
 }
 
